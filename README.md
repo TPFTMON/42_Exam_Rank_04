39671b2ac1ae9441d2602960d8331fec95f2fdf0
TPFTMON <tpftmoon@gmail.com>
2025-09-16 12:05:47 +0200

    preparations for exam learning

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # 42_exam_rank_04
 My preparations and the finished task for the exam in the future
+
+The subject that will be on the exam or very similar to that is in subject.en.txt.
+
+I want to try to do it without using: malloc, free, signal, kill
---
+++ microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaryshe <abaryshe@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/15 17:51:51 by abaryshe          #+#    #+#             */
+/*   Updated: 2025/09/16 12:05:20 by abaryshe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <sys/wait.h>
+# include <string.h>
+
+#endif
